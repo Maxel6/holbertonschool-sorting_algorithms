3ce41ea6ded90b23492302c4982d6d74e10fabed
Axel Malka <axelmalka@outlook.fr>
2023-05-22 14:20:10 +0000

    adding sort function for selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * selection_sort - sort an array using selection sorting algo
+ * @array: the array to sort
+ * @size: the size of the array
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int min = array[i], tmp = 0, lmin = 0;
+
+	if (!array || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = i + 1, min = array[i]; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				lmin = j;
+			}
+		}
+		tmp = array[i];
+		array[lmin] = tmp;
+		array[i] = min;
+		print_array(array, size);
+	}
+}
