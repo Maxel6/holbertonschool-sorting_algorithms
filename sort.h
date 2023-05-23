91ac3dd70c05b02c528ffe3d9f314d86096f3846
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-23 11:03:09 +0200

    update header file with new prototype function for task 3

--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 listint_t *create_listint(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+size_t partition(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
+void compare_sort(int *array, int low, int high, size_t size);
 
 #endif
