68106ccb4d3ffecbadcf0f14298283ae40a12ae6
Axel Malka <axelmalka@outlook.fr>
2023-05-23 08:49:48 +0000

    fix insertion sort function

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sort a list with insertion sort algo
+ * @list: The list to be printed
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current, *insertion_point;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+    current = (*list)->next;
+    while (current != NULL)
+    {
+        insertion_point = current->prev;
+        while (insertion_point != NULL && insertion_point->n > current->n)
+        {
+            insertion_point->next = current->next;
+            if (current->next != NULL)
+                current->next->prev = insertion_point;
+            current->prev = insertion_point->prev;
+            current->next = insertion_point;
+            insertion_point->prev = current;
+            if (current->prev != NULL)
+                current->prev->next = current;
+            else
+                *list = current;
+            insertion_point = current->prev;
+            print_list(*list);
+        }
+        current = current->next;
+    }
+}
